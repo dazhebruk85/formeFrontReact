d244820f9c76b6b2a570d1a25f6263ff5af152dd
Агапов Иван <I.Agapov@bssys.com>
2018-11-26 15:31:18 +0300

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import LoginPage from './loginPage/LoginPage'
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-
+import { HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -11,9 +10,9 @@ class App extends Component {
       <Router basename={process.env.PUBLIC_URL}>
         <div style={{width:'100%',height:'100%', backgroundColor:'#f2f2f2'}}>
             <Switch>
-                  <Route exact path= "/" render={() => (<Redirect to="/front/loginPage"/>)}/>
-                  <Route exact path= "/front" render={() => (<Redirect to="/front/loginPage"/>)}/>
-                  <Route exact path='/front/loginPage' component={LoginPage} />
+                  <Route exact path="/" render={() => (<Redirect to="/front/loginPage"/>)}/>
+                  <Route exact path="/front" render={() => (<Redirect to="/front/loginPage"/>)}/>
+                  <Route exact path="/front/loginPage" component={LoginPage} />
             </Switch>
       </div>
     </Router>
