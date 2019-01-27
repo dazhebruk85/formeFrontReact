a11382115da8134f5cc0a8ea5d298069dcd559a5
AgaII <I.Agapov@bssys.com>
2019-01-27 14:25:14 +0300

    RepairAppRooms

--- src/Const.js
+++ src/Const.js
@@ -1,5 +1,5 @@
 // System consts
-export const WORK_APP_URL = 'http://10.1.53.185:1313/back/FrontServlet';
+export const WORK_APP_URL = 'http://10.1.53.185:9191/back/FrontServlet';
 export const HOME_APP_URL = 'http://192.168.0.104:1313/back/FrontServlet';
 export const APP_URL = WORK_APP_URL;
 
